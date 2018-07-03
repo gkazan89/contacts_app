b0e1ae41ec37aae683dde0fac0e7af273485c159
Grant Kazan <grantkazan@gmail.com>
2018-07-02 19:41:28 -0500

    cleaned up routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/contact_url" => "contacts#contact_method" 
-  get "/all_contacts_url" => "contacts#all_contacts_method"
   get "/contacts" => "contacts#index"
   get "/contacts/:id" => "contacts#show"
   post "/contacts" => "contacts#create"
